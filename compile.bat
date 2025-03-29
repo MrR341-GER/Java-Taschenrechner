03bf8956bc97bc2cbdf10a34dbae66c0e6536c61
pasqual.vestner <pasqual.vestner@artif.com>
2025-03-29 15:35:43 +0100

    moving src files
    Signed-off-by: pasqual.vestner <pasqual.vestner@artif.com>

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 wmic process where "commandline like '%%GrafischerTaschenrechner%%'" delete
-javac -d bin GrafischerTaschenrechner.java
+javac -sourcepath src -d bin src\GrafischerTaschenrechner.java
 jar cvfe GrafischerTaschenrechner.jar GrafischerTaschenrechner -C bin .
 java -jar GrafischerTaschenrechner.jar
