39bd75ddf763fbc0ca9fd06b10eed71a7ec79701
pasqual.vestner <pasqual.vestner@artif.com>
2025-03-29 13:21:47 +0100

    Syntaxfehler behoben
    Signed-off-by: pasqual.vestner <pasqual.vestner@artif.com>

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-wmic process where "commandline like '%GrafischerTaschenrechner%'" delete
+wmic process where "commandline like '%%GrafischerTaschenrechner%%'" delete
 javac -d bin GrafischerTaschenrechner.java
 jar cvfe GrafischerTaschenrechner.jar GrafischerTaschenrechner -C bin .
 java -jar GrafischerTaschenrechner.jar
